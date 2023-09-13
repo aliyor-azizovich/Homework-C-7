b17b2b2a8be9c292e21028093a56a5f7c2caf30e
Aliyor Azizovich <aliyor.0276@gmail.com>
2023-09-13 09:51:31 +0500

    First commit

---
+++ Task_64/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbersFromNTo1(n);
+
+        Console.ReadKey();
+    }
+
+    static void PrintNumbersFromNTo1(int n)
+    {
+        if (n == 0) // базовый случай
+        {
+            return;
+        }
+        else // рекурсивный случай
+        {
+            Console.Write(n + " ");
+            PrintNumbersFromNTo1(n - 1);
+        }
+    }
+}
---
+++ Task_68/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+
+        Console.WriteLine("Результат функции Аккермана для m={0} и n={1} равен {2}", m, n, result);
+    }
+}
+
+
+// Функция Аккермана определяется следующим образом:
+// - A(m, n) = n + 1, если m = 0
+// - A(m, n) = A(m - 1, 1), если m > 0 и n = 0
+// - A(m, n) = A(m - 1, A(m, n - 1)), если m > 0 и n > 0
+
+//    !!!!!!!!!!!!!!!!!!!!!!!!!!
+// ВАЖНО!!!
+// Функция Аккермана растет очень быстро, и уже при небольших значениях m и n может вызвать переполнение стека
+//  из-за большой глубины рекурсии. 
+// Это может привести к ошибке "StackOverflowException".
